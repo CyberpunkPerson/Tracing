0a2629282c6800ac569e0fb7ead652101330f208
Andrew Belunov <cyberpunk.person@gmail.com>
2021-10-26 16:04:15 +0300

    News handle integration (#1)

---
+++ src/main/java/com/github/cyberpunkperson/tracing/configuration/kafka/properties/KafkaTopicInformation.java
@@ -0,0 +1,10 @@
+package com.github.cyberpunkperson.tracing.configuration.kafka.properties;
+
+import lombok.Data;
+
+@Data
+public class KafkaTopicInformation {
+
+    private String name;
+
+}
---
+++ src/main/java/com/github/cyberpunkperson/tracing/domain/source/marvel/configuration/MarvelConfiguration.java
@@ -0,0 +1,43 @@
+package com.github.cyberpunkperson.tracing.domain.source.marvel.configuration;
+
+import com.github.cyberpunkperson.tracing.helper.integration.converter.ProtoEventMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.dsl.IntegrationFlow;
+import org.springframework.integration.dsl.MessageChannels;
+import org.springframework.integration.kafka.inbound.KafkaMessageDrivenChannelAdapter;
+import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.messaging.MessageChannel;
+import src.main.java.com.github.cyberpunkperson.tracing.marvel.kafka.event.Marvel.Event;
+
+import static org.springframework.integration.dsl.IntegrationFlows.from;
+
+@Configuration
+@EnableIntegration
+class MarvelConfiguration {
+
+    @Bean
+    KafkaMessageDrivenChannelAdapter<String, byte[]> inboundMarvelChannelAdapter(KafkaMessageListenerContainer<String, byte[]> marvelContainer,
+                                                                                 MessageChannel integrationErrorChannel) {
+        var inboundAdapter = new KafkaMessageDrivenChannelAdapter<>(marvelContainer);
+        inboundAdapter.setPayloadType(byte.class);
+        inboundAdapter.setBindSourceRecord(true);
+        inboundAdapter.setMessageConverter(new ProtoEventMessageConverter<>(Event.newBuilder().build().getParserForType()));
+        inboundAdapter.setErrorChannel(integrationErrorChannel);
+        return inboundAdapter;
+    }
+
+    @Bean
+    MessageChannel inboundMarvelChannel() {
+        return MessageChannels.publishSubscribe().get();
+    }
+
+    @Bean
+    IntegrationFlow inboundMarvelFlow(KafkaMessageDrivenChannelAdapter<String, byte[]> inboundMarvelChannelAdapter,
+                                      MessageChannel inboundMarvelChannel) {
+        return from(inboundMarvelChannelAdapter)
+                .channel(inboundMarvelChannel)
+                .get();
+    }
+}
